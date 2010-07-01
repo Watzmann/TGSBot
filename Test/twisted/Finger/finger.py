f3ccb2d764bd1c4d6b6e64a41890535687215e3e
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-07-01 19:17:57 +0000

    Finger/fingers.py:		fingers06

--- Test/twisted/Finger/finger.py
+++ Test/twisted/Finger/finger.py
@@ -9,11 +9,14 @@ from twisted.protocols import basic
 
 class FingerProtocol(basic.LineReceiver):
     def lineReceived(self, user):
-        self.transport.write("No such user\r\n")
+        self.transport.write(self.factory.getUser(user)+"\r\n")
         self.transport.loseConnection()
 
 class FingerFactory(protocol.ServerFactory):
     protocol = FingerProtocol
+
+    def getUser(self, user):
+        return "No such user"
 
 reactor.listenTCP(1079, FingerFactory())
 reactor.run()
