cdc8f74092839b363689947d5d2da384e0cfa6f9
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-07-18 12:40:53 +0000

    Test/twisted/protocols.py:	erste Erweiterungen für SIBS Test/twisted/twisted-proxy.py:	erste Erweiterungen für SIBS

--- Test/twisted/protocols.py
+++ Test/twisted/protocols.py
@@ -16,20 +16,39 @@ class Echo(Protocol):
             self.transport.loseConnection()
 
     def connectionMade(self):
-        self.id = self.factory.numProtocols = self.factory.numProtocols+1
+        self.id = self.factory.incNumProtocols()
         msg = 'sei gegruesst, nummer %d\r\n' % self.id
-        print msg
         self.transport.write('server %d: %s' % (self.id,msg))
+        print 'had %d connections so far :)' % self.factory.maxProtocols
         if self.factory.numProtocols > 1001:
             print 'wegen ueberfuellung geschlossen'
             self.transport.write("Too many connections, try later")
             self.transport.loseConnection()
 
     def connectionLost(self, reason):
-        self.factory.numProtocols = self.factory.numProtocols-1
+        self.factory.decNumProtocols()
         print 'aus die maus', self.id
 
 class QOTD(Protocol):
     def connectionMade(self):
         self.transport.write("An apple a day keeps the doctor away\r\n")
         self.transport.loseConnection()
+
+class CLIP(Echo):
+    def __init__(self,):
+        self.brocken = ''
+        
+    def dataReceived(self, data):
+        if len(data) == 1:
+            self.brocken = data
+        elif len(self.brocken) == 1:
+            data = self.brocken + data
+            self.brocken = ''
+        if len(data) > 1:
+            print 'heard:', data
+            if data.lower().startswith('quit'):
+                print 'lasse die Verbindung %d fallen' % self.id
+                self.transport.loseConnection()
+            result = self.factory.parse(data)
+            self.transport.write('echo %d: %s\r\n' % (self.id,result))
+
