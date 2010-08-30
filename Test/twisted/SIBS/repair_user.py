2b8de30b741e64c92be02ae2b90527eefdb02453
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-08-30 11:42:30 +0000

    SIBS/repair_user.py:		migrate user data (user db)

---
+++ Test/twisted/SIBS/repair_user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Reparaturen/Migrationen in der Users-DB."""
+
+from persistency import Persistent, Db
+
+def login_time(key, value):
+    print key, value.passwd, value.rating, value.login, value.host
+    value.login = int(value.login)
+    print key, value.passwd, value.rating, value.login, value.host
+    print
+    
+def for_all_users(db, repair):
+    keys = db.keys()
+    for e,k in enumerate(keys):
+        repair(k, db[k])
+    db.sync()
+    
+if __name__ == '__main__':
+    db = Db('db/users')
+    repair = login_time
+    for_all_users(db.db, repair)
+    db.close()
