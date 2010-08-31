b08525e83ce04da4036d946a1858dc2a604b90aa
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-08-31 13:56:04 +0000

    SIBS/version.py:		New way of generating version string

--- Test/twisted/SIBS/version.py
+++ Test/twisted/SIBS/version.py
@@ -2,27 +2,46 @@
 # -*- coding: utf-8 -*-
 """Version of SIBS."""
 
-revs = []
+##from sibs_utils import REV
+##from persistency import REV
+##from sibs_user import REV
+##from telnet_server import REV
+##from clip import REV
+##from command import REV
 
-from sibs_utils import REV
-revs.append(REV)
-from persistency import REV
-revs.append(REV)
-from sibs_user import REV
-revs.append(REV)
-from telnet_server import REV
-revs.append(REV)
-from clip import REV
-revs.append(REV)
-from command import REV
-revs.append(REV)
-
+VERSION = '0.1'
 REV = '$Revision$'
-revs.append(REV)
 
-revs = [int(i.strip('$').split(':')[1].strip()) for i in revs]
-max_rev = str(max(revs))
-VERSION = '.'.join(['0.1',max_rev])
+class Version:
+    
+    __shared_state = {}     # Borg Pattern
+                            # http://code.activestate.com/recipes/66531-singleton-we-dont-need-no-stinkin-singleton-the-bo/
+
+    def __init__(self,):
+        self.__dict__ = self.__shared_state
+        if not hasattr(self, 'revisions'):
+            self.revisions = {}
+
+    def register(self, module, rev):
+        self.revisions[module] = int(rev.strip('$').split(':')[1].strip())
+
+    def version(self, module=None):
+        if module is None:
+            max_rev = str(max(self.revisions.values()))
+            return '.'.join([VERSION,max_rev])
+        else:
+            revs = self.revisions
+            if revs.has_key(module):
+                v = '.'.join([VERSION,str(revs[module])])
+                return 'module %s: %s' % (module, v)
+            else:
+                return ''
+
+v = Version()
+v.register(__name__, REV)
 
 if __name__ == "__main__":
-    print VERSION
+    print 'module name:', __name__
+    print v.version()
+    print v.version(1)
+    print v.version(__name__)
