081db7d1b4596f271489ac2b0dad11e139a3c23c
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-10-03 12:35:01 +0000

    SIBS/states.py:			The State-Machine (under construction)

--- Test/twisted/SIBS/states.py
+++ Test/twisted/SIBS/states.py
@@ -11,30 +11,66 @@ v = Version()
 v.register(__name__, REV)
 
 class State:
+    """Base class for states in this state machine."""
+    
     def __init__(self,):
         self.active = False
 
     def activate(self, player, params):
-        pass
+        """Activates this state.
+    player:     class Player
+    params:     **kw
+    """
+        self.player = player
+        self.approved_player = self.player  # This may have to be overwritten
+                                            # in special(); may be the opponent.
+        self.params = params    # TODO: ich glaub, ich brauch die nur in activate()
+                                #       also kein Attribut dieses Objects
+        self.results = None
+        self.active = True
+        self._special()
+        self._chat()
 
     def deactivate(self,):
-        pass
-
-    def state_check(self,):
-        pass
+        self.active = False
 
     def action(self, player, cmd):
-        self.result = actions[cmd]
+        if self._state_check(player, cmd):      # action is allowed, only,
+            self._action(player, cmd)           # when state_check is passed
+            self._transit()
 
-    def transit(self, next_state):
+    def _state_check(self, player, cmd):
+        if (player == self.approved_player) and actions.has_key(cmd):
+            #self._action(player, cmd)
+        # TODO: hier einen qualifizierten Fehler zurückgeben oder (vermutlich) ''
+
+    def _action(self, player, cmd):
+        action = actions[cmd]          # set attribute instead of return to be
+        self.result = action(player, self.params) # able to do some processing
+        
+    def _transit(self, next_state):
         self.deactivate()
-        next_state.activate(player, params) # die params kommen von der action
+        next_state.activate(player, self.result) # parameters come from _action
 
-    def chat(self,):
+    def _special(self,):
+        """Method intended for being overwritten. _special() is being
+    called during activation phase.
+    """
         pass
 
-    def special(self,):
+    def _chat(self,):
+        """Send messages to the players during activation phase. This Method
+    is intended for being overwritten.
+    """
         pass
+
+class Started(State):
+    """State A: game has started."""
+    
+    def __init__(self,):
+        self.name = 'started'
+        State.__init__(self)
+
 
 class StateMachine:
     def __init__(self,):
