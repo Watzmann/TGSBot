b4fd8cd115e68d05fa17df22db64c1fc49dafceb
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-10-04 06:35:06 +0000

    SIBS/states.py:			The State-Machine (under construction)

--- Test/twisted/SIBS/states.py
+++ Test/twisted/SIBS/states.py
@@ -10,13 +10,24 @@ from version import Version
 v = Version()
 v.register(__name__, REV)
 
+global VERBOSE
+VERBOSE = False
+
+def talk(msg):
+    if VERBOSE:
+        print msg
+        
 class State:
     """Base class for states in this state machine."""
     
-    def __init__(self,):
+    def __init__(self, actions):
         self.active = False
+        if actions is None:
+            self.actions = {}
+        else:
+            self.actions = dict(zip(actions, ({'action': None},)*len(actions)))
 
-    def activate(self, player, params):
+    def activate(self, player, **params):
         """Activates this state.
     player:     class Player
     params:     **kw
@@ -28,24 +39,33 @@ class State:
                                 #       also kein Attribut dieses Objects
         self.results = None
         self.active = True
+        self.machine(self)
         self._special()
         self._chat()
 
     def deactivate(self,):
         self.active = False
 
-    def action(self, player, cmd):
-        if self._state_check(player, cmd):      # action is allowed, only,
-            self._action(player, cmd)           # when state_check is passed
-            self._transit()
+    def action(self, player, cmd, **params):
+        check = self._state_check(player, cmd)
+        if check == '':                         # action is allowed, only,
+            self._action(player, cmd, **params) # when state_check is passed
+            self._transit(self.actions[cmd]['follow_up'])
+        else:
+            self._chat(check)
 
     def _state_check(self, player, cmd):
-        if (player == self.approved_player) and actions.has_key(cmd):
-            #self._action(player, cmd)
+        if (player == self.approved_player):
+            if self.actions.has_key(cmd):
+                return ''
+            else:
+                return "error: you can't %s" % cmd
+        else:
+            return "error: it is not your turn to %s" % cmd
         # TODO: hier einen qualifizierten Fehler zurückgeben oder (vermutlich) ''
 
     def _action(self, player, cmd):
-        action = actions[cmd]          # set attribute instead of return to be
+        action = actions[cmd]['action'] # set attribute instead of return to be
         self.result = action(player, self.params) # able to do some processing
         
     def _transit(self, next_state):
@@ -58,30 +78,119 @@ class State:
     """
         pass
 
-    def _chat(self,):
+    def _chat(self, msg=None):
         """Send messages to the players during activation phase. This Method
     is intended for being overwritten.
     """
-        pass
+        if not msg is None:
+            print msg
 
-class Started(State):
+class GameStarted(State):
     """State A: game has started."""
     
-    def __init__(self,):
-        self.name = 'started'
-        State.__init__(self)
+    def __init__(self, actions,):
+        self.name = 'game_started'
+        State.__init__(self, actions)
 
+class TurnStarted(State):
+    """State B: a new turn has started."""
+    
+    def __init__(self, actions,):
+        self.name = 'turn_started'
+        State.__init__(self, actions)
+
+    def _special(self,):
+        self.player = 1
+        #switch players
+
+class Doubled(State):
+    """State C: the cube has been turned."""
+    
+    def __init__(self, actions,):
+        self.name = 'doubled'
+        State.__init__(self, actions)
+
+class Taken(State):
+    """State D: the cube has been taken."""
+    
+    def __init__(self, actions,):
+        self.name = 'taken'
+        State.__init__(self, actions)
+
+class Rolled(State):
+    """State E: dice have been rolled."""
+    
+    def __init__(self, actions,):
+        self.name = 'rolled'
+        State.__init__(self, actions)
+
+class Moved(State):
+    """State F: move has been made."""
+    
+    def __init__(self, actions,):
+        self.name = 'moved'
+        State.__init__(self, actions)
+
+class GameFinished(State):
+    """State G: game has ended."""
+    
+    def __init__(self, actions,):
+        self.name = 'finished'
+        State.__init__(self, actions)
 
 class StateMachine:
     def __init__(self,):
-        cube = {'player_on_turn': None, 'next': }
+        self.states = dict((        # TODO: es sieht etwas inkonsequent aus,
+                                    # hier die actions anzulegen, sie dann aber
+                                    # weiter unten zu erweitern;
+                                    # das kann man doch alles unten machen, dann
+                                    # gibt es keine Abhängigkeiten von Code
+            ('game_started', GameStarted(('start',))),
+            ('turn_started', TurnStarted(('roll', 'double'))),
+            ('doubled', Doubled(('take', 'pass'))),
+            ('taken', Taken(('roll',))),
+            ('rolled', Rolled(('move', 'cant_move',))),
+            ('moved', Moved(('turn', 'win'))),
+            ('finished', GameFinished(None)),
+            ))
+        s = self.states
+        model = {'game_started': (('start', (s['rolled'], True)),),
+                 'turn_started': (('roll', (s['rolled'], False)),
+                                  ('double', (s['doubled'], False)),),
+                 'doubled': (('take', (s['taken'], False)),
+                             ('pass', (s['finished'], False)),),
+                 'taken': (('roll', (s['rolled'], True)),),
+                 'rolled': (('move', (s['moved'], False)),
+                            ('cant_move', (s['turn_started'], True)),),
+                 'moved': (('turn', (s['taken'], False)),
+                             ('win', (s['finished'], True)),),
+                }
+        for s in model:
+            for k,v in model[s]:
+                self.states[s].actions[k]['follow_up'] = v[0]
+                self.states[s].actions[k]['auto'] = v[1]
+            self.states[s].machine = self.activate
 
-    def next_state(self,):
-        pass
-    
+    def start(self, player, **kw):
+        self.states['game_started'].activate(player, **kw)
+
+    def action(self, player, cmd, **kw):
+        self.active.action(player, cmd, **kw)
+
+    def activate(self, state):
+        self.active = state
+        talk('%s: aktiviert (%s)' % (state.name, state.__doc__))
+
+    def done():
+        return self.active.name == 'finished'
+
+from game import Player
+
 if __name__ == '__main__':
+    VERBOSE = True
+    p1 = Player('white', 'user1', None, 0)
+    p2 = Player('black', 'user2', p1, 0)
+    p1.opponent = p2
     s = StateMachine()
-    for c in sys.argv[1:]:
-        for h in h.help_(c):
-            print h,
-            
+    s.start(p1)
+    s.action(p1, 'move', move='18-12 12-10')
