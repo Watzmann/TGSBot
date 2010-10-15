1fd5c7ee7e70911c2648ee7da68ddb585cea1e5b
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-10-15 19:54:04 +0000

    SIBS/repair_user.py:		finishing up "autoroll"

--- Test/twisted/SIBS/repair_user.py
+++ Test/twisted/SIBS/repair_user.py
@@ -5,15 +5,26 @@
 from persistency import Persistent, Db
 from sibs_user import Info
 
+def data(v):
+    return (v.login, v.host, v.name, v.passwd, v.rating, v.experience)            
+
 def login_time(db, key, value):
     print key, value.passwd, value.rating, value.login, value.host
     value.login = int(value.login)
     print key, value.passwd, value.rating, value.login, value.host
     print
 
-def messages(db, key, v):
-    data = (v.login,v.host, v.name, v.passwd, v.rating, v.experience)            
-    info = Info(data, v.toggles, v.settings, [])
+def messages(db, key, value):
+    vdata = data(value)
+    info = Info(vdata, v.toggles, v.settings, [])
+    db[key] = info
+    
+def autoroll(db, key, value):
+    vdata = data(value)
+    toggles = value.toggles
+    if (not 'autoroll' in toggles) and ('double' in toggles):
+        toggles['autoroll'] = not toggles['double']
+    info = Info(vdata, toggles, value.settings, value.messages)
     db[key] = info
     
 def for_all_users(db, repair):
@@ -26,5 +37,6 @@ if __name__ == '__main__':
     db = Db('db/users')
     #repair = login_time
     #repair = messages
+    #repair = autoroll
     for_all_users(db.db, repair)
     db.close()
