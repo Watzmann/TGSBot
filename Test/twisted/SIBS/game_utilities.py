20c4020d96af12fc6cac4321023267160e43cc15
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-11-20 18:50:06 +0000

    SIBS/code_statistics/statistics:	----------- SIBS/game_utilities.py:		implementing check_rolls() SIBS/tests/test_utilities.py:	unittests utilities

--- Test/twisted/SIBS/game_utilities.py
+++ Test/twisted/SIBS/game_utilities.py
@@ -74,50 +74,183 @@ def check_roll_old(dice, position, nr_bar, direction):
 def check_roll(dice, position, bar_nr, direction):
     """Checks for possible moves depending on 'dice'."""
     exhausted = False
-    list_of_moves = []  # TODO: lieber dict?
-    pos = position      # TODO: vielleicht bei position bleiben
+    #list_of_moves = []  # TODO: lieber dict?
+    #pos = position      # TODO: vielleicht bei position bleiben
     d1, d2 = dice
-    my_dice = list(dice[:])
-    nr_of_moves = {True:4, False:2}[d1 == d2]
-    # -------------------------------------------enter from the bar
+    if d1 == d2:
+        nr_of_moves = 4
+        my_dice = [d1,] * 4
+    else:
+        nr_of_moves = 2        
+        my_dice = list(dice[:])
+    list_of_moves = []
+    nr_moved_pieces = 0
+    forced_move = False
+    checks_neccessary = True
+    # ------------------------------------------- enter from the bar
     bar_moves = min(nr_of_moves, bar_nr)
     if bar_moves:
         ret = check_bar_moves(dice, position, bar_moves, direction['bar'])
-        # ret {'remains': der rest,}
-    # -------------------------------------------
-    if bar_moves > 0:
+        my_dice = ret['my_dice']
+        list_of_moves = ret['list_of_moves']
+        forced_move = ret['forced_move']
+        checks_neccessary = ret['checks_neccessary']
+        nr_moved_pieces = ret['nr_moved_pieces']
+        nr_of_moves = ret['remaining_moves']
+    # ------------------------------------------- moves in the board
+    if checks_neccessary:
+        print '+++++++++++++', nr_of_moves
+        ret = check_board_moves(tuple(my_dice), position[1:-1], nr_of_moves,
+                                                        direction['bar'])
+        print '##### ret', ret
+        # TODO  not (if len(dice) > 2 elif (len(dice) == 2) if dice[0] == dice[1])
+        #       and if forced move oder vielleicht nicht alle gesetzt
+        #       dann noch mal mit vertauschten dice durch check_board_moves
+    if nr_moved_pieces < nr_of_moves:
         nr_of_moves = len(list_of_moves)
-        exhausted = True
-    if exhausted:
         logger.info('der spieler kann nur %d zuege ziehen' % nr_of_moves)
-    return {'nr_pieces': nr_of_moves, 'list_of_moves': list_of_moves,}
+    return {'nr_pieces': nr_moved_pieces, 'list_of_moves': list_of_moves,
+            'forced_move': forced_move}
 
-def check_bar_moves(position, nr_bar_moves, bar):
-    list_of_moves = []  # TODO: lieber dict?
-    #pos = position      # TODO: vielleicht bei position bleiben
+def check_board_moves(dice, position, nr_of_moves, bar):
+    list_of_moves = []
+    forced_move = True
+    remaining_moves = nr_of_moves
+    for d in dice:
+        found = 0
+        if bar == 25:
+            for e,p in enumerate(position):
+                if p > 0:
+                    if (e >= d) and (position[e-d] > -2):
+                        list_of_moves.append('%d-%d' % (e+1,e+1-d))
+                        if not found:
+                            position[e] -= 1
+                            position[e-d] += 1
+                        remaining_moves -= 1
+                        found += 1
+                        logger.debug('found: wurf %d   point %d,%d  move %s   ' \
+                                     '(%s) Forced move: %s' % \
+                                     (d, e, p, list_of_moves[-1],
+                                      list_of_moves, forced_move))
+                if (found > 1) or ((found == 1) and not forced_move):
+                    forced_move = False
+                    break
+        elif bar == 0:
+            for e,p in enumerate(position):
+                if p < 0:
+                    if (e+d < 24) and (position[e+d] < 2):
+                        list_of_moves.append('%d-%d' % (e+1,e+1+d))
+                        if not found:
+                            position[e] -= 1
+                            position[e+d] += 1
+                        remaining_moves -= 1
+                        found += 1
+                if (found > 1) or ((found == 1) and not forced_move):
+                    forced_move = False
+                    break
+        else:
+            logger.error("Falsche Belegung vom 'Bar'-Feld (%d)!" % bar)
+    nr_moved_pieces = min(nr_of_moves, nr_of_moves - remaining_moves)
+    if found == 0:
+        forced_move = True
+    return {'list_of_moves': list_of_moves,
+            'nr_moved_pieces': nr_moved_pieces,
+            'forced_move': forced_move,}
+    
+def check_bar_moves(dice, position, nr_bar_moves, bar):
+    list_of_moves = []
     d1, d2 = dice
     my_dice = list(dice[:])
-    nr_of_moves = {True:4, False:2}[d1 == d2]
-    if d1 == d2:
+    forced_move = False
+    if d1 == d2:        # it's a double
         if bar == 25:
             p = bar - d1
-            if position[p] > -2:
-                list_of_moves = ['bar-%d' % p,]*bar_moves
-                my_dice = [my_dice[0],]*(4-bar_moves)
-                bar_moves = 0
-                logger.info('pasch getested: bar %d  wurf %d   point %d   ' \
-                     'checker %d  (%s) (%s) (%d)' % \
-                     (bar,d,p,pos[p],list_of_moves,my_dice,bar_moves))
-                break
+            move_possible = position[p] > -2
         elif bar == 0:
             p = bar + d1
-            if pos[p] < 2:
-                list_of_moves = ['bar-%d' % p,]*bar_moves
-                my_dice = [my_dice[0],]*(4-bar_moves)
-                bar_moves = 0
-                logger.info('pasch getested: bar %d  wurf %d   point %d   ' \
-                     'checker %d  (%s) (%s) (%d)' % \
-                     (bar,d,p,pos[p],list_of_moves,my_dice,bar_moves))
-                break
-    ret = {'remains': der rest,}
+            move_possible = position[p] < 2
+        else:
+            logger.error("Falsche Belegung vom 'Bar'-Feld (%d)!" % bar)
+        if move_possible:
+            nr_moved_pieces = min(nr_bar_moves, 4)
+            remaining_moves = 4 - nr_moved_pieces
+            forced_move = remaining_moves == 0
+            checks_neccessary = remaining_moves > 0
+            list_of_moves = ['bar-%d' % p,] * nr_moved_pieces
+            my_dice = [d1,] * remaining_moves
+        else:
+            nr_moved_pieces = 0
+            remaining_moves = 0
+            forced_move = True 
+            checks_neccessary = False
+            my_dice = []
+        logger.debug('pasch getested: bar %d  wurf %d   point %d   ' \
+             'checker %d  (%s) (%s) (%d) # %d Forced move: %s  check %s' % \
+             (bar, d1, p, position[p], list_of_moves, my_dice,
+              remaining_moves, nr_moved_pieces, forced_move, checks_neccessary))
+    else:               # normal roll
+        remaining_moves = 2
+        if bar == 25:
+            for d in dice:
+                p = bar - d
+                if position[p] > -2:
+                    list_of_moves.append('bar-%d' % p)
+                    my_dice.remove(d)
+                    remaining_moves -= 1
+                logger.debug('normal testing: bar %d  wurf %d   point %d   ' \
+                     'checker %d  (%s) (%s) (%d)  Forced move: %s' % \
+                     (bar, d, p, position[p], list_of_moves, my_dice,
+                      remaining_moves, forced_move))
+        elif bar == 0:
+            for d in dice:
+                p = bar + d
+                if position[p] < 2:
+                    list_of_moves.append('bar-%d' % p)
+                    my_dice.remove(d)
+                    remaining_moves -= 1
+                logger.debug('normal testing: bar %d  wurf %d   point %d   ' \
+                     'checker %d  (%s) (%s) (%d)  Forced move: %s' % \
+                     (bar, d, p, position[p], list_of_moves, my_dice,
+                      remaining_moves, forced_move))
+        else:
+            logger.error("Falsche Belegung vom 'Bar'-Feld (%d)!" % bar)
+        if (nr_bar_moves > 1) or (len(list_of_moves) == 0):
+            forced_move = True 
+            checks_neccessary = False
+            remaining_moves = 0
+            nr_moved_pieces = len(list_of_moves)
+            my_dice = []
+        else:
+            forced_move = True 
+            checks_neccessary = True
+            remaining_moves = 1
+            nr_moved_pieces = 1
+            if my_dice == []:
+                my_dice = list(dice[:])
+                forced_move = False 
+        logger.debug('normal getested: bar %d  wurf %d   point %d   ' \
+             'checker %d  (%s) (%s) (%d) # %d Forced move: %s  check %s' % \
+             (bar, d, p, position[p], list_of_moves, my_dice,
+              remaining_moves, nr_moved_pieces, forced_move, checks_neccessary))        
+    ret = {'my_dice': my_dice, 'list_of_moves': list_of_moves,
+           'nr_moved_pieces': nr_moved_pieces, 'remaining_moves': remaining_moves,
+           'forced_move': forced_move, 'checks_neccessary': checks_neccessary}
     return ret
+
+if __name__ == '__main__':
+    data = [{'dice':[(6,6), (5,5), (3,3), (6,5), (6,2), (2,1),],
+             'pos': [0, 0,0,0,1,4,5, 0,3,0,0,0,0,
+                     0,0,0,2,0,0, -7,-5,-3,0,0,0, 0],
+             'dir': {'home':0, 'bar':25}, 'bar': [0,1,2,]},
+        ]
+    for d in data:
+        pos = d['pos']
+        direction = d['dir']
+        print 'Testing position', pos, '  ->', direction, '='*10
+        for b in d['bar']:
+            print '-'*40, 'bar', b
+            for die in d['dice']:
+                print '-'*20, 'dice', die
+                print check_roll(die, pos, b, direction)
+                print
+        
---
+++ Test/twisted/SIBS/tests/test_utilities.py
@@ -0,0 +1,342 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+u"""Unittests for the game_utilities module."""
+
+import sys
+import os
+sibs_dir = os.path.dirname(os.getcwd())
+os.chdir(sibs_dir)
+sys.path.append(sibs_dir)
+import unittest
+from test_tools import *
+from game_utilities import *
+import logging
+
+logger.setLevel(logging.ERROR)
+
+class TestBarMovesOnly(unittest.TestCase):
+
+    def setUp(self):
+        self.position_25 = [0, 0,0,0,1,4,5, 0,3,0,0,0,0,
+                            0,0,0,2,0,0, -7,-5,-3,0,0,0, 0]
+
+##    def tearDown(self):
+
+    def testcheck_bar_1_66(self):
+        ret = check_bar_moves((6,6), self.position_25, 1, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_1_55(self):
+        ret = check_bar_moves((5,5), self.position_25, 1, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_1_33(self):
+        ret = check_bar_moves((3,3), self.position_25, 1, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [3, 3, 3])
+        self.assertEqual(ret['list_of_moves'], ['bar-22'])
+        self.assertEqual(ret['nr_moved_pieces'], 1)
+        self.assertEqual(ret['remaining_moves'], 3)
+        self.assertEqual(ret['forced_move'], False)
+        self.assertEqual(ret['checks_neccessary'], True)
+
+    def testcheck_bar_1_65(self):
+        ret = check_bar_moves((6,5), self.position_25, 1, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_1_62(self):
+        ret = check_bar_moves((6,2), self.position_25, 1, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [6,])
+        self.assertEqual(ret['list_of_moves'], ['bar-23'])
+        self.assertEqual(ret['nr_moved_pieces'], 1)
+        self.assertEqual(ret['remaining_moves'], 1)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], True)
+
+    def testcheck_bar_1_21(self):
+        ret = check_bar_moves((2,1), self.position_25, 1, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [2, 1])
+        self.assertEqual(ret['list_of_moves'], ['bar-23', 'bar-24'])
+        self.assertEqual(ret['nr_moved_pieces'], 1)
+        self.assertEqual(ret['remaining_moves'], 1)
+        self.assertEqual(ret['forced_move'], False)
+        self.assertEqual(ret['checks_neccessary'], True)
+
+    def testcheck_bar_2_66(self):
+        ret = check_bar_moves((6,6), self.position_25, 2, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_2_55(self):
+        ret = check_bar_moves((5,5), self.position_25, 2, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_2_33(self):
+        ret = check_bar_moves((3,3), self.position_25, 2, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [3, 3])
+        self.assertEqual(ret['list_of_moves'], ['bar-22', 'bar-22'])
+        self.assertEqual(ret['nr_moved_pieces'], 2)
+        self.assertEqual(ret['remaining_moves'], 2)
+        self.assertEqual(ret['forced_move'], False)
+        self.assertEqual(ret['checks_neccessary'], True)
+
+    def testcheck_bar_2_65(self):
+        ret = check_bar_moves((6,5), self.position_25, 2, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_2_62(self):
+        ret = check_bar_moves((6,2), self.position_25, 2, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-23'])
+        self.assertEqual(ret['nr_moved_pieces'], 1)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_2_21(self):
+        ret = check_bar_moves((2,1), self.position_25, 2, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-23', 'bar-24'])
+        self.assertEqual(ret['nr_moved_pieces'], 2)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_3_66(self):
+        ret = check_bar_moves((6,6), self.position_25, 3, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_3_55(self):
+        ret = check_bar_moves((5,5), self.position_25, 3, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_3_33(self):
+        ret = check_bar_moves((3,3), self.position_25, 3, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [3,])
+        self.assertEqual(ret['list_of_moves'], ['bar-22', 'bar-22', 'bar-22'])
+        self.assertEqual(ret['nr_moved_pieces'], 3)
+        self.assertEqual(ret['remaining_moves'], 1)
+        self.assertEqual(ret['forced_move'], False)
+        self.assertEqual(ret['checks_neccessary'], True)
+
+    def testcheck_bar_3_65(self):
+        ret = check_bar_moves((6,5), self.position_25, 3, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_3_62(self):
+        ret = check_bar_moves((6,2), self.position_25, 3, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-23'])
+        self.assertEqual(ret['nr_moved_pieces'], 1)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_3_21(self):
+        ret = check_bar_moves((2,1), self.position_25, 3, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-23', 'bar-24'])
+        self.assertEqual(ret['nr_moved_pieces'], 2)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_4_66(self):
+        ret = check_bar_moves((6,6), self.position_25, 4, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_4_55(self):
+        ret = check_bar_moves((5,5), self.position_25, 4, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_4_33(self):
+        ret = check_bar_moves((3,3), self.position_25, 4, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-22', 'bar-22', 'bar-22', 'bar-22'])
+        self.assertEqual(ret['nr_moved_pieces'], 4)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_4_65(self):
+        ret = check_bar_moves((6,5), self.position_25, 4, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_4_62(self):
+        ret = check_bar_moves((6,2), self.position_25, 4, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-23'])
+        self.assertEqual(ret['nr_moved_pieces'], 1)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_4_21(self):
+        ret = check_bar_moves((2,1), self.position_25, 4, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-23', 'bar-24'])
+        self.assertEqual(ret['nr_moved_pieces'], 2)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_5_66(self):
+        ret = check_bar_moves((6,6), self.position_25, 5, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_5_55(self):
+        ret = check_bar_moves((5,5), self.position_25, 5, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_5_33(self):
+        ret = check_bar_moves((3,3), self.position_25, 5, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-22', 'bar-22', 'bar-22', 'bar-22'])
+        self.assertEqual(ret['nr_moved_pieces'], 4)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_5_65(self):
+        ret = check_bar_moves((6,5), self.position_25, 5, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], [])
+        self.assertEqual(ret['nr_moved_pieces'], 0)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_5_62(self):
+        ret = check_bar_moves((6,2), self.position_25, 5, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-23'])
+        self.assertEqual(ret['nr_moved_pieces'], 1)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+    def testcheck_bar_5_21(self):
+        ret = check_bar_moves((2,1), self.position_25, 5, 25)
+        self.assertEqual(len(ret), 6)
+        self.assertEqual(ret['my_dice'], [])
+        self.assertEqual(ret['list_of_moves'], ['bar-23', 'bar-24'])
+        self.assertEqual(ret['nr_moved_pieces'], 2)
+        self.assertEqual(ret['remaining_moves'], 0)
+        self.assertEqual(ret['forced_move'], True)
+        self.assertEqual(ret['checks_neccessary'], False)
+
+
+# testcases
+##
+##bar: ein checker draußen, kommt rein
+##     ein checker draußen, kommt nicht rein
+##     zwei checker draußen, kommt rein
+##     zwei checker draußen, einer kommt rein
+##     zwei checker draußen, kommt nicht rein
+##     drei checker draußen, ....
+##     genauso für den gegnerischen spieler
+
+if __name__ == "__main__":
+    do_suites = (len(sys.argv) > 1) and (sys.argv[1] == 'suites')
+
+    if not do_suites:
+        run_test(TestBarMoves('testcheck_bar_1_66'))
+    else:
+        run_suites(globals())
