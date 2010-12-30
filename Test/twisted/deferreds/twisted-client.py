ea281c34ab59ec84999d1386d8456e4fe1bf4c12
Andreas Hausmann <ahausmann_2003@yahoo.de>
2010-12-30 15:03:22 +0000

    deferreds/twisted-client.py:	fehlerhaftes input nach Ende entfernt

--- Test/twisted/deferreds/twisted-client.py
+++ Test/twisted/deferreds/twisted-client.py
@@ -30,44 +30,21 @@ class Greeter(Protocol):
 class Communicator:
     def communicate(self,):  #p, d):
         """Do a bit of manual communication with the server. Quit with a keyword."""
-##        while True:
         s = raw_input('give me more >> ')
-        if s.lower() in ('bye', 'exit', 'quit'):
-            self.protocol.transport.loseConnection()
         return s
-##                break
-##            else:
-##                self.protocol.sendMessage(s)
-##                d.callback(s)
-
-##    def get_communicator(p):
-##        d = defer.Deferred()
-##        reactor.callLater(2, communicate, p, d)
-##        return d
 
     def gotProtocol(self, p):
-    ##    d = get_communicator(p) # communication with server is deferred
-    ##    print 'defer', d
-    ##    def onError(err):
-    ##        return 'Internal error in communication'
-    ##    d.addErrback(onError)
-    ##    
-    ##    def writeResponse(message):
-    ##        print 'sending', message
-    ##        p.sendMessage(message)
-    ##    d.addCallback(writeResponse)
-    ##
-    ##    p.sendMessage("Hello")
-    ##    reactor.callLater(1, p.sendMessage, "This is sent in a second")
         print 'got protocol'
         self.protocol = p
         print 'start communication'
-##        com.communicate()
         self.input_loop()
 
     def send(self, msg):
-        self.protocol.sendMessage(msg)
-        self.input_loop()
+        if msg.lower() in ('bye', 'exit', 'quit'):
+            self.protocol.transport.loseConnection()
+        else:
+            self.protocol.sendMessage(msg)
+            self.input_loop()
 
     def input_loop(self,):
         d = threads.deferToThread(self.communicate)
