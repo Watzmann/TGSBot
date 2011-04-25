297575a3cd364648293d45645c25758947542087
Andreas Hausmann <ahausmann_2003@yahoo.de>
2011-04-25 12:11:57 +0000

    Test/twisted/protocols.py:	Weiterentwicklung Test/twisted/client/README:	Übersicht über client

--- Test/twisted/protocols.py
+++ Test/twisted/protocols.py
@@ -8,17 +8,23 @@ from twisted.internet.protocol import Protocol
 ##from twisted.python import log
 
 class Echo(Protocol):
+    """Protocol Echo"""
     def dataReceived(self, data):
         print 'heard:', data
-        self.transport.write('echo %d: %s\r\n' % (self.id,data))
+        self.transport.write('echo %s: %s\r\n' % (self.id,data))
         if data.startswith('exit'):
-            print 'lasse die Verbindung %d fallen' % self.id
+            print 'lasse die Verbindung %s fallen' % self.id
             self.transport.loseConnection()
+        if data.startswith('change'):
+            print 'wechsle das Protokoll zu Quiet'
+            # So kommt man nicht in ein anderes Protokoll. Erst der nächste
+            # bekommt das neue Protokoll untergejubelt.
+            self.factory.protocol = Quiet
 
     def connectionMade(self):
-        self.id = self.factory.incNumProtocols()
-        msg = 'sei gegruesst, nummer %d\r\n' % self.id
-        self.transport.write('server %d: %s' % (self.id,msg))
+        self.id = '(%s) %d' % (id(self),self.factory.incNumProtocols())
+        msg = 'sei gegruesst, nummer %s\r\n' % self.id
+        self.transport.write('server %s: %s' % (self.id,msg))
         print 'had %d connections so far :)' % self.factory.maxProtocols
         if self.factory.numProtocols > 2001:
             print 'wegen ueberfuellung geschlossen'
@@ -29,6 +35,18 @@ class Echo(Protocol):
         self.factory.decNumProtocols()
         print 'aus die maus', self.id
 
+class Quiet(Echo):
+    """Protocol Quiet"""
+    def dataReceived(self, data):
+        print 'heard:', data
+        self.transport.write('quiet %s: %s\r\n' % (self.id,data))
+        if data.startswith('exit'):
+            print 'lasse die Verbindung %s fallen' % self.id
+            self.transport.loseConnection()
+        if data.startswith('change'):
+            print 'wechsle das Protokoll zu Echo'
+            self.factory.protocol = Echo
+    
 class QOTD(Protocol):
     def connectionMade(self):
         self.transport.write("An apple a day keeps the doctor away\r\n")
