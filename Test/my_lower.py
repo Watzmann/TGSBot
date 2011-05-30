852800815dd67f519d2cacb8c3c8f32769f883c5
Andreas Hausmann <ahausmann_2003@yahoo.de>
2011-05-30 16:23:59 +0200

    slight changes
    Test/my_lower.py:	testing a method for converting umlaute to lower

---
+++ Test/my_lower.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+umlaute = ((u'Ä', u'ä'), (u'Ö', u'ö'), (u'Ü', u'ü'))
+
+def my_lower(string):
+    low = string.lower().decode('latin-1')
+    ret = list(low)
+    for s,t in umlaute:
+        while s in ret:
+            idx = ret.index(s)
+            ret[idx] = t
+    return ''.join(ret)
+
+examples = (
+    'Böse',
+    'Bärtig',
+    'Büste',
+    'ÖSTLICH',
+    'ÜBEL',
+    'ÄRGER',
+    )
+
+if __name__ == '__main__':
+    for s in examples:
+        r = s.decode('utf-8').encode('latin-1')
+        print s, '->', my_lower(r)
