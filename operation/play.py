2ea342bf69935184f516f7b6c3f82d3d201b768b
Andreas Hausmann <ahausmann_2003@yahoo.de>
2012-07-27 23:28:39 +0200

    Bot: simplifying Join a bit.

--- operation/play.py
+++ operation/play.py
@@ -1,18 +1,56 @@
 """Commands about toggles, settings, etc."""
 
-from operation.basics import Request
+from operation.basics import Request, Response
 
 class Join(Request):
-    def __init__(self, dispatch, manage,):
-        self.expected = "You start"
-        #** You are now playing a 7 point match with hannes.
-        #You are now playing with hannes. Your running match was loaded.
+
+    def __init__(self, dispatch, manage, opp, ML):
+        self.expected = self.expected_answer(opp, ML)
         Request.__init__(self, dispatch, manage,)
 
+    def expected_answer(self, opponent, ML):
+        if ML is None:
+            i_expect = "You are now playing with %s. " \
+                       "Your running match was loaded." % opponent
+        else:
+            i_expect = "** You are now playing a %s " \
+                       "point match with %s." % (ML, opponent)
+        return i_expect
+
     def received(self, message):
-##        print 'TESTE -------------------', message
-        expected_answer = self.expected.test(message)
-##        print 'FINDE -------------------', expected_answer
+        expected_answer = self.expected == message[0]
         if expected_answer:
             self.purge()
         return expected_answer
+
+class Play(Request):
+
+    class Answer(Response):
+
+        def __init__(self, opp, ML):
+            Response.__init__(self, '')
+            self.complex_answer(self.join_answer, [opp, ML])
+
+        def join_answer(self, expected, message):
+            opponent = expected[0]
+            ML = expected[1]
+            if ML is None:
+                i_expect = "You are now playing with %s. " \
+                           "Your running match was loaded." % opponent
+            else:
+                i_expect = "** You are now playing a %s " \
+                           "point match with %s." % (ML, opponent)
+            return message[0] == i_expect
+
+    def __init__(self, dispatch, manage, opp, ML):
+        self.expected = self.Answer(opp, ML)
+        Request.__init__(self, dispatch, manage,)
+
+    def received(self, message):
+        expected_answer = self.expected.test(message)
+        if expected_answer:
+            self.purge()
+        return expected_answer
+
+    def update(self,):
+        self.manage['default'] = self
