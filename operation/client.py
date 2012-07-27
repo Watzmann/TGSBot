90d1b9cd371a44f6d16b003878d2d151032a0c5a
Andreas Hausmann <ahausmann_2003@yahoo.de>
2012-07-27 15:50:25 +0200

    Bot: command Set. Setting boardstyle.      Processing double messages.

--- operation/client.py
+++ operation/client.py
@@ -2,7 +2,8 @@
 from operation.basics import Request
 from operation.welcome import Welcome
 from operation.welcome import Login
-from operation.settings import Toggle, Join
+from operation.settings import Toggle, Set
+from operation.play import Join
 from operation.config import ADMINISTRATORS, COMMANDS
 
 class Dispatch:
@@ -16,6 +17,10 @@ class Dispatch:
 
     def send_server(self, message):
         self.protocol.sendMessage(message)
+
+    def set_boardstyle(self,):
+        settings = Set(self, self.requests)
+        settings.send_command('set')
 
     def query_status(self,):
         toggle = Toggle(self, self.requests)
@@ -42,33 +47,39 @@ class Dispatch:
 
     def parse(self, message):
         lines = message.splitlines()
-        personal = lines[0].split()
-        print 'PERSONAL', personal
+        cmd_line = lines[0].split()
+        print 'COMMAND LINE', cmd_line
         message_done = False
-        if lines[0] in self.requests:
-            print 'ICH CHECKE -----------', lines[0]
-            # TODO: das funktioniert noch nicht.
-            #       Versuch mal den bot auf 'away' zu setzen und dann zu starten
-            #       Er bekommt die "away"-message als erste Zeile
-            request = self.requests.pop(lines[0])
-            request.receive(message)
-            message_done = True
-        elif 'default' in self.requests:
-            print 'IN DEFAULT -----------'
-            request = self.requests['default']
-            message_done = request.receive(message)
-        if not message_done:
-            if message.startswith('12 '):
-                #print 'command (1):', message
-                self.command(message[3:])
-            elif len(personal) > 3 and \
-                 ' '.join(personal[1:3]) == "wants to":
-                opponent = personal[0]
-                if personal[3] == 'play':
-                    ML = int(personal[5])
+        checks = [0]
+        for e,l in enumerate(lines[1:],1):
+            if l in self.requests:
+                checks.append(e)
+        for c in checks:
+            if lines[c] in self.requests:
+                print 'ICH CHECKE -----------', lines[0]
+                # TODO: das funktioniert noch nicht.
+                #       Versuch mal den bot auf 'away' zu setzen und dann zu starten
+                #       Er bekommt die "away"-message als erste Zeile
+                request = self.requests.pop(lines[c])
+                request.received(lines[c:])
+                message_done = True
+            elif 'default' in self.requests:
+                print 'IN DEFAULT -----------'
+                request = self.requests['default']
+                message_done = request.received(lines[c:])
+            if not message_done:
+                if message.startswith('12 '):
+                    #print 'command (1):', message
+                    self.command(message[3:])
+                elif len(cmd_line) > 3 and \
+                     ' '.join(cmd_line[1:3]) == "wants to":
+                    opponent = cmd_line[0]
+                    if cmd_line[3] == 'play':
+                        ML = int(cmd_line[5])
+                        print 'joining a %s point match with %s' % (ML, opponent)
+                    else:
+                        ML = None
+                        print 'resuming a match with %s' % opponent
+                    self.join(opponent)
                 else:
-                    ML = None
-                print 'joining a %s point match with %s' % (ML, opponent)
-                self.join(opponent)
-            else:
-                print 'got from server: >%s<' % message
+                    print 'got from server: >%s<' % '\n'.join(lines[c:])
--- operation/settings.py
+++ operation/settings.py
@@ -3,20 +3,42 @@
 from operation.basics import Request
 
 class Toggle(Request):
+    """Query the toggles. From the response set dispatch.toggles.
+
+  Explanation of mechanics:
+
+  1) Upon instanciation the expected server answer string is set to
+       self.expected = "The current settings are:".
+     This takes effect in Request.__init__ as it is stored to 'manage'.
+
+  2) The dispatcher may invoke the 'toggle'-command as it likes.
+
+  3) When parsing the dispatcher detects the expected server answer in
+       if lines[0] in self.requests:
+     and calls the requests received() method handing over the full response.
+
+  4) Toggle.received() now parses the message and sets the toggles variable.
+
+  5) If toggle 'ready' is 'off', then it now is set 'on'.
+     This is indeed a bad side effect; maybe it is not wanted; setting the bot
+     'ready off' is not possible.
+"""
     def __init__(self, dispatch, manage,):
         self.expected = "The current settings are:"
         Request.__init__(self, dispatch, manage,)
 
-    def receive(self, message):
-        lines = message.splitlines()
+    def received(self, message):
         self.toggles = {}
+        print 'TOGGLE: READ MY LINES', message
         try:
-            for t in lines[1:]:
+            for t in message[1:]:
+                print 'TOGGLE: working on >%s<' % t
                 s = t.split()
                 self.toggles[s[0]] = s[-1]
         except:
             raise RuntimeError('Toggle got unexpected data: >%s<' % message)
         self.dispatch.toggles = self.toggles
+        print 'TOGGLE: FIND READY', self.toggles['ready']
         if self.toggles['ready'] == 'NO':
             self.set_ready()
         self.purge()
@@ -25,17 +47,27 @@ class Toggle(Request):
         self.dispatch.send_server('toggle ready')
         self.dispatch.toggles['ready'] = 'YES'
 
-class Join(Request):
+class Set(Request):
     def __init__(self, dispatch, manage,):
-        self.expected = "You start"
-        #** You are now playing a 7 point match with hannes.
-        #You are now playing with hannes. Your running match was loaded.
+        self.expected = "Settings of variables:"
         Request.__init__(self, dispatch, manage,)
 
-    def receive(self, message):
-##        print 'TESTE -------------------', message
-        expected_answer = self.expected.test(message)
-##        print 'FINDE -------------------', expected_answer
-        if expected_answer:
-            self.purge()
-        return expected_answer
+    def received(self, message):
+        self.settings = {}
+        print 'SET: READ MY LINES', message
+        try:
+            for t in message[1:8]:
+                print 'SET: working on >%s<' % t
+                s = t.split()
+                key = s[0].rstrip(':')
+                self.settings[key] = s[-1]
+        except:
+            raise RuntimeError('Set got unexpected data: >%s<' % message)
+        self.dispatch.settings = self.settings
+        if self.settings['boardstyle'] != '3':
+            self.set_boardstyle()
+        self.purge()
+
+    def set_boardstyle(self,):
+        self.dispatch.send_server('set boardstyle 3')
+        self.dispatch.settings['boardstyle'] = '3'
