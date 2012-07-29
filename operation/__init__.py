a7f1c67057f55322065a55f5314be7d3fe22299b
Andreas Hausmann <ahausmann_2003@yahoo.de>
2012-07-29 16:28:13 +0200

    Bot: Logging; starting play.

--- operation/__init__.py
+++ operation/__init__.py
@@ -1,12 +1,2 @@
 """Classes that constitute the abilities of the bot."""
 
-##import sys
-##import os
-##
-##packet_dir = os.getcwd()
-##print packet_dir
-##if not packet_dir in sys.path:
-##    sys.path.insert(0,packet_dir)
-##    print 'kommt rein'
-##else:
-##    print 'is schon drin'
