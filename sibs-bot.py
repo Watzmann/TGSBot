93fbd136aaf55885da915cf3bec7b724912b0df6
Andreas Hausmann <ahausmann_2003@yahoo.de>
2012-08-23 23:17:36 +0200

    Bot: turn to reconnecting client.

--- sibs-bot.py
+++ sibs-bot.py
@@ -4,7 +4,7 @@
 Basiert auf client/twisted-client1.py. Das Reconnecting-Zeugs ist raus.
 """
 
-from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.internet.protocol import Protocol, ReconnectingClientFactory
 from twisted.internet import reactor, defer
 from twisted.python import log
 import sys
@@ -45,21 +45,23 @@ class Com(Protocol):
         log.msg('dropConnection', logLevel=TRACE)
         self.transport.loseConnection()
 
-class ComClientFactory(ClientFactory):
-
-    protocol = Com
-
+class ComClientFactory(ReconnectingClientFactory):
     def startedConnecting(self, connector):
         log.msg('Started to connect.', logLevel=TRACE)
-        #reactor.callLater(10, reactor.stop)
+
+    def buildProtocol(self, addr):
+        log.msg('Connected to %s:%s.' % (options.host, options.port), logLevel=TRACE)
+        log.msg('Resetting reconnection delay.', logLevel=VERBOSE)
+        self.resetDelay()
+        return Com()
 
     def clientConnectionLost(self, connector, reason):
         log.msg('Lost connection. Reason: %s' % reason, logLevel=logging.INFO)
-        reactor.callWhenRunning(reactor.stop)
+        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
 
     def clientConnectionFailed(self, connector, reason):
         log.msg('Connection failed. Reason: %s' % reason, logLevel=logging.INFO)
-        reactor.callWhenRunning(reactor.stop)
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
 
 def usage(progname):
     usg = """usage: %prog [<gid>]
